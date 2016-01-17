484f3fb53d0d8ccdc81957954ace640b6e23e261
David Ladowitz <david@ladowitz.com>
2016-01-17 12:40:29 -0800

    flash

--- storm.rb
+++ storm.rb
@@ -8,35 +8,51 @@ require 'audite'
 @back  = @client.lights[1]
 @right = @client.lights[2]
 
-def strobe(cycles)
-  cycles.times do
-    rand(3..10).times do
-      light = @client.lights[rand(0..2)]
-      light.set_state({bri: rand(150..254), on: true, transitiontime: rand(0..3)})
-      light.set_state({:on => false, color_temperature: 153, transitiontime: rand(0..3)})
-    end
+def strobe(strike_distance)
+  puts "* Lightning Strike *"
+  puts "Strike was #{strike_distance} miles away."
+
+
+  brightness = (254/strike_distance).round(0)
+  puts "Brightness is #{brightness}"
+  if brightness >= 245
+    puts "Flash was 100%"
+  else
+    puts "Flash was #{((brightness/254.0) * 100).round(0)}%"
+  end
+
+  rand(3..10).times do
+    light = @client.lights[rand(0..2)]
+    light.set_state({bri: brightness, on: true, transitiontime: rand(0..3)})
+    light.set_state({:on => false, color_temperature: 153, transitiontime: rand(0..3)})
   end
 end
 
-def thunder
+def thunder(strike_distance)
+  puts "\n* Thunder Clap *"
+  volume_coefficient = 2
+  volume = (volume_coefficient/strike_distance).round(2)
+  puts "Volume was #{volume}db"
+
   thunder_files = ["thunder1.mp3", "thunder2.mp3", "thunder3.mp3", "thunder4.wav"]
   audio_file = thunder_files[rand(thunder_files.length)]
 
-  system "afplay #{audio_file}"
+  system "afplay #{audio_file} -v volume"
 end
 
 
 while true do
-  puts "\n"
-  puts "------------Lightning Strike-------------------"
-  strobe(1)
-  sleep(rand(0..3))
+  puts "\n -------------------------------"
+  strike_distance = (rand(0.0..2.0)).round(3)
 
-  puts "Thunder Clap"
-  thunder
+  strobe(strike_distance)
 
-  sleep_time = rand(0..300)
-  puts "Sleeping for #{(sleep_time/60.0).round(1)} minutes"
-  sleep(sleep_time)
-  puts "-------------Waking-------------------------"
+  sleep(strike_distance)
+
+  thunder(strike_distance)
+
+  strike_interval = rand(0..10)
+  puts "\nNext strike in #{(strike_interval/60.0).round(2)} minutes"
+  sleep(strike_interval)
+  puts "-----------------------------------"
 end
